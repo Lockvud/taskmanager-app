5435597f98d4e3dae57b9217ad87da0eb4edcaef
Nikita Kasyanov <casyanov.nickita@gmail.com>
2025-07-23 13:44:39 +0300

    Добавлено приложение tasks и REST Framework в настройки

--- backend/settings.py
+++ backend/settings.py
@@ -14,3 +14,15 @@ DATABASES = {
         'PORT': config('POSTGRES_PORT'),
     }
 }
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'tasks',
+]
