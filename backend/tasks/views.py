59e5daefcc7b5ba5647fe6cc8b29ef29c04e27ce
Nikita Kasyanov <casyanov.nickita@gmail.com>
2025-07-25 00:51:37 +0300

    Внесены изменения и реализован API для задач через DRF

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    return JsonResponse({'message': 'Добро пожаловать в Task Manager API'})
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
+
