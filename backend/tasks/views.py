7a2b2ca8bdd5b52145b21be7a6663aaab29287ea
Nikita Kasyanov <casyanov.nickita@gmail.com>
2025-07-27 21:10:57 +0300

    Внесены изменения в views.py

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -9,4 +9,13 @@ def home(request):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')
     serializer_class = TaskSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['created_at']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        is_completed = self.request.query_params.get('is_completed')
+        if is_completed in ['true', 'false']:
+            queryset = queryset.filter(is_completed=(is_completed == 'true'))
+        return queryset
 
