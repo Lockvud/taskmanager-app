98145ccc450b61ab53b2629cd69d060110b0f220
Nikita Kasyanov <casyanov.nickita@gmail.com>
2025-07-27 20:35:46 +0300

    Добавлена логика редактирования задачи через App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <main class="container">
-    <h1>Список задач</h1>
-    <TaskForm @created="loadTasks" />
-    <TaskList :tasks="tasks" />
-  </main>
+  <h1>Список задач</h1>
+  <div class="container">
+    <TaskForm
+      :editingTask="editingTask"
+      @created="loadTasks"
+      @updated="loadTasks"
+    />
+    <TaskList :tasks="tasks" @edit="setEditingTask" />
+  </div>
 </template>
 
 <script setup>
-import TaskList from './components/TaskList.vue'
-import TaskForm from './components/TaskForm.vue'
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
+import TaskForm from './components/TaskForm.vue'
+import TaskList from './components/TaskList.vue'
 
 const tasks = ref([])
+const editingTask = ref(null)
 
 const loadTasks = async () => {
-  const response = await axios.get('http://localhost:8000/api/tasks/')
-  tasks.value = response.data
+  const res = await axios.get('http://localhost:8000/api/tasks/')
+  tasks.value = res.data
+  editingTask.value = null
+}
+
+const setEditingTask = (task) => {
+  editingTask.value = task
 }
 
 onMounted(loadTasks)
 </script>
+
 
 <style>
 .container {
