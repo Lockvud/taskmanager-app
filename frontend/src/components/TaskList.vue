4e95eb2c38f3709249ee07b6578f850398a2e0a4
Nikita Kasyanov <casyanov.nickita@gmail.com>
2025-07-27 20:33:39 +0300

    Добавлена кнопка редактирования задачи в TaskList.vue

--- frontend/src/components/TaskList.vue
+++ frontend/src/components/TaskList.vue
@@ -1,22 +1,33 @@
 <template>
-  <div>
-    <ul v-if="tasks.length">
-      <li v-for="task in tasks" :key="task.id">
-        {{ task.title }} - {{ task.status }}
-      </li>
-    </ul>
-    <p v-else>Задачи не найдены.</p>
-  </div>
+  <ul class="task-list">
+    <li v-for="task in tasks" :key="task.id" class="task-item">
+      <h3>{{ task.title }}</h3>
+      <p>{{ task.description }}</p>
+      <button @click="$emit('edit', task)">Редактировать</button>
+    </li>
+  </ul>
 </template>
 
+
 <script setup>
-import { ref, onMounted } from 'vue';
-import api from '../api.js';
-
-const tasks = ref([]);
-
-onMounted(async () => {
-  const response = await api.get('tasks/');
-  tasks.value = response.data;
-});
+defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  }
+})
 </script>
+
+<style scoped>
+.task-list {
+  list-style: none;
+  padding: 0;
+}
+
+.task-item {
+  padding: 1rem;
+  border: 1px solid #ccc;
+  margin-bottom: 1rem;
+  border-radius: 4px;
+}
+</style>
