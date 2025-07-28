10aa10eef2b33b2b94df151d4ad11c4567a19f0a
Nikita Kasyanov <casyanov.nickita@gmail.com>
2025-07-29 01:00:09 +0300

    Добавлена поддержка Swagger (drf-yasg) и роуты /swagger/ и /redoc/

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from tasks.views import TaskViewSet
-from tasks.views import home
+from tasks.views import TaskViewSet, home
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Task Manager API",
+      default_version='v1',
+      description="API для управления списком задач",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -11,4 +23,7 @@ urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
